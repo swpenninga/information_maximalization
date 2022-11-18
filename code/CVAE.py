eb3173188a9bb54450f945da13ea733543ad8d01
Simon Penninga <simon-p@live.nl>
2022-11-18 17:07:41 +0100

    finalize VAE

--- code/CVAE.py
+++ code/CVAE.py
@@ -5,51 +5,59 @@ import torch.nn.functional as F
 
 
 class Encoder(nn.Module):
-    def __init__(self):
+    def __init__(self,num_z):
         super(Encoder, self).__init__()
         self.conv1 = nn.Conv2d(1, 16, 3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(16, 16, 3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(16, 16, 3, stride=1, padding=1)
-        self.conv4 = nn.Conv2d(16, 1, 3, stride=1, padding=1)
+        self.fc1 = nn.Linear(256, 32)
+        self.fc2 = nn.Linear(32, int(2*num_z))
 
     def forward(self, x):
-        # use the created layers here
         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
         x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
         x = F.max_pool2d(F.relu(self.conv3(x)), (2, 2))
-        x = F.max_pool2d(self.conv4(x), (2, 4))
+        x = F.relu(self.fc1(torch.flatten(x, start_dim=1)))
+        x = self.fc2(x)
         return x
 
 
 # %%  Decoder
 class Decoder(nn.Module):
-    def __init__(self):
+    def __init__(self,num_z):
         super(Decoder, self).__init__()
-        self.conv5 = nn.ConvTranspose2d(1, 16, 3, stride=1, padding=1)
-        self.conv6 = nn.ConvTranspose2d(16, 16, 3, stride=1, padding=1)
-        self.conv7 = nn.ConvTranspose2d(16, 16, 3, stride=1, padding=1)
-        self.conv8 = nn.ConvTranspose2d(16, 1, 3, stride=1, padding=1)
-        self.upsample1 = nn.Upsample(scale_factor=(2, 4), mode='nearest')
-        self.upsample2 = nn.Upsample(scale_factor=2, mode='nearest')
+        self.conv1 = nn.ConvTranspose2d(16, 16, 3, stride=1, padding=1)
+        self.conv2 = nn.ConvTranspose2d(16, 16, 3, stride=1, padding=1)
+        self.conv3 = nn.ConvTranspose2d(16, 1, 3, stride=1, padding=1)
+        self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        self.fc1 = nn.Linear(num_z, 32)
+        self.fc2 = nn.Linear(32, 256)
 
     def forward(self, x):
-        x = self.upsample1(F.relu(self.conv5(x)))
-        x = self.upsample2(F.relu(self.conv6(x)))
-        x = self.upsample2(F.relu(self.conv7(x)))
-        x = self.upsample2(self.conv8(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.upsample(F.relu(self.conv1(x.reshape([int(x.size()[0]), 16, 4, 4]))))
+        x = self.upsample(F.relu(self.conv2(x)))
+        x = self.upsample(self.conv3(x))
         return x
 
 
 # %%  Autoencoder
 class AE(nn.Module):
-    def __init__(self):
+    def __init__(self,num_z):
         super(AE, self).__init__()
-        self.encoder = Encoder()
-        self.decoder = Decoder()
+        self.encoder = Encoder(num_z)
+        self.decoder = Decoder(num_z)
 
     def forward(self, x):
-        z = self.encoder(x)
+        h = self.encoder(x)
+        z = sample(h)
         xhat = self.decoder(z)
-        return xhat, z
+        return xhat, h, z
 
 
+def sample(h):
+    z = torch.empty(h.size()[0], int(h.size()[1]/2))
+    for i in range(int(h.size()[1]/2)):
+        z[:, i] = h[:, i] + torch.mul(torch.randn(h.shape[0]), torch.exp(h[:, int(i+h.size()[1]/2)]))
+    return z
--- code/main.py
+++ code/main.py
@@ -1,8 +1,5 @@
 import argparse
 import torch
-from torchvision import transforms
-from torchvision.datasets import MNIST
-from torch.utils.data import DataLoader
 import torch.nn as nn
 from tqdm import tqdm
 import matplotlib.pyplot as plt
@@ -22,78 +19,97 @@ def main(args):
     data_loc = 'D://5LSL0-Datasets'
     batch_size = 64
     train_loader, test_loader = MNIST_dataloader.create_dataloaders(data_loc, batch_size)
-
-    cvae = CVAE.AE()
+    num_z = 3
+    cvae = CVAE.AE(num_z)
     optimizer = torch.optim.Adam(cvae.parameters(), lr=args.learning_rate)
     if args.train:
         cvae = trainnet(cvae, train_loader, test_loader, optimizer)
     else:
-        cvae.load_state_dict(torch.load("models/model153616"))
+        cvae.load_state_dict(torch.load("models/model170516"))
 
     if args.plot:
-        plotting(cvae, test_loader.dataset.Clean_Images, test_loader.dataset.Labels)
+        plotting(cvae, test_loader.dataset.Clean_Images + 1, test_loader.dataset.Labels, num_z)
 
 
-def plotting(cvae, data, labels, fig_size=(10, 10)):
+def plotting(cvae, data, labels, num_z, fig_size=(10, 10)):
     cvae.eval()
-    xhat, z = cvae(data)
+    xhat, h, z = cvae(data)
     z = z.detach().numpy()
-
-    plt.figure(figsize=fig_size)
-    plt.scatter(z[:, 0, 0, 0], z[:, 0, 1, 0], c=labels, cmap='tab10')
-    plt.colorbar()
-    plt.show()
+    fig = plt.figure(figsize=fig_size)
+    if num_z == 3:
+        ax = fig.add_subplot(projection='3d')
+        ax.scatter(z[:, 0], z[:, 1], z[:, 2], c=labels, cmap='tab10')
+    else:
+        ax = fig.add_subplot()
+        ax.scatter(z[:, 0], z[:, 1], c=labels, cmap='tab10')
+    # ax.colorbar()
+    fig.show()
 
 
 def trainnet(cvae, train_loader, test_loader, optimizer):
+    criterion = nn.MSELoss()
     for epoch in range(args.epochs):
 
         totalloss = 0.0
-        totalvalloss = 0.0
+
         print(f"\nTraining Epoch {epoch + 1}:")
         cvae.train()
 
         for idx, (x, _, labels_x) in enumerate(tqdm(train_loader)):
             optimizer.zero_grad()
-            xhat, z = cvae(x)
-            loss = loss_fn(xhat, x, z[:, 0, 0, 0], z[:, 0, 1, 0])
+            xhat, h, z = cvae(x+1)
+            loss, MSE, KLD = loss_fn(xhat, x+1, h, criterion)
             loss.backward()
             optimizer.step()
             totalloss += loss.item()
             if idx % 100 == 99:
-                print(f"loss:[{totalloss/100:.4f}]")
+                print(f"loss:[{totalloss/100:.2e}] MSE:[{MSE:.2e}] KLD:[{KLD.item():.2e}]")
                 totalloss = 0.0
 
         cvae.eval()
+        totalvalloss = 0.0
         for idx, (x, _, _) in enumerate(test_loader):
-            xhat, z = cvae(x)
-            loss = loss_fn(xhat, x, z[:, 0, 0, 0], z[:, 0, 1, 0])
+            xhat, h, z = cvae(x)
+            loss, MSE, KLD = loss_fn(xhat, x+1, h, criterion)
             totalvalloss += loss.item()
+            if idx % 50 == 49:
+                print(f"loss:[{totalvalloss/50:.2e}] MSE:[{MSE:.2e}] KLD:[{KLD.item():.2e}]")
+                totalvalloss = 0.0
         cvae.train()
-        print(f'val_loss:[{totalvalloss / len(test_loader):.4f}]')
 
+    save_model(cvae)
+
+    return cvae
+
+def save_model(cvae):
     now = datetime.now()
     current_time = now.strftime("%H%M%S")
     torch.save(cvae.state_dict(), "models/model" + current_time)
     print("Saved model weights as model" + current_time + ".")
+    return
 
-    return cvae
 
-def loss_fn(x_hat, x, mean, log_var):
+# 1e-6 works for z_dim=2
+# 1e-5 works for z_dim=3
+
+
+def loss_fn(x_hat, x, h, criterion, beta=1e-5):
     # BCE = torch.nn.functional.binary_cross_entropy(x_hat, x, reduction='sum')
-    criterion = nn.MSELoss()
     MSE = criterion(x_hat, x)
-    KLD = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
-    return (MSE + 0.001*KLD) / x.size(0)
+    KLD = torch.zeros(1)
+    for i in range(int(h.size()[1]/2)):
+        KLD += torch.sum(torch.square(h[:, i]) - 2*h[:, i+int(h.size()[1]/2)] + torch.square(torch.exp(h[:, i+int(h.size()[1]/2)])) - 1)
+        # KLD += -0.5 * torch.sum(1 + h[:, i] - h[:, i+int(h.size()[1]/2)].pow(2) - h[:, i].exp())
+    return (MSE + beta*KLD)/x.size()[0], MSE/x.size()[0], beta*KLD/x.size()[0]
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--seed", type=int, default=0)
-    parser.add_argument("--epochs", type=int, default=1)
+    parser.add_argument("--seed", type=int, default=1523423)
+    parser.add_argument("--epochs", type=int, default=2)
     parser.add_argument("--learning_rate", type=float, default=0.001)
     parser.add_argument("--plot", type=bool, default=True)
-    parser.add_argument("--train", type=bool, default=False)
+    parser.add_argument("--train", type=bool, default=True)
 
     args = parser.parse_args()
 
